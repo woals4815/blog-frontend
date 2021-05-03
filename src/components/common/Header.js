5bda8b350c4cad2ecdf97f88e6dae46c50a4af7d
Jayden JaeMin Jeong <71315300+woals4815@users.noreply.github.com>
2021-05-03 13:58:06 +0900

    Edit Button components for link and add Header, reponsive component, Header, HeaderContainer

---
+++ src/components/common/Header.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import Button from "./Button";
+import Responsive from "./Responsive";
+
+const HeaderBlock = styled.div`
+    position: fixed;
+    width: 100%;
+    background: white;
+    box-shadow: 0px 2px 4px rgba(0,0,0,0.08);
+`;
+
+const Wrapper = styled(Responsive)`
+    height: 4rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .logo {
+        font-size: 1.125rem;
+        font-weight: 800;
+        letter-spacing: 2px;
+    }
+    .right{
+        display: flex;
+        align-items: center;
+    }
+`;
+
+const Spacer = styled.div`
+    height: 4rem;
+`;
+
+const UserInfo = styled.div`
+    font-weight: 800;
+    margin-right: 1rem;
+`;
+
+const Header = ({user}) => {
+    return (
+        <>
+            <HeaderBlock>
+                <Wrapper>
+                    <Link to="/" className="logo">
+                        REACTERS
+                    </Link>
+                    {user? (
+                        <div className="right">
+                            <UserInfo>{user.username}</UserInfo>
+                            <Button>Log Out</Button>
+                        </div>
+                    ): (
+                        <div className="right">
+                            <Button to="/login">Log In</Button>
+                        </div>
+                    )}
+                </Wrapper>
+            </HeaderBlock>
+            <Spacer />
+        </>
+    )
+};
+
+
+export default Header;
--- src/pages/PostListPage.js
+++ src/pages/PostListPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Button from '../components/common/Button';
+import HeaderContainer from "../containers/common/HeaderContainer";
 
 const PostListPage = () => {
   return (
-    <div>
-      <Button>Press</Button>
-    </div>
+    <>
+      <HeaderContainer />
+      <div>Hi.</div>
+    </>
   );
 };
 
